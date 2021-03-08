4b463c9eeed7a43a8baeb6a739955ac45c354131
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-03-08 21:30:34 +0900

    add vue router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import Home from "./pages/Home.vue";
+import Admin from "./pages/Admin.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: Admin,
+    },
+  ],
+});
+
+createApp(App).use(router).mount("#app");
