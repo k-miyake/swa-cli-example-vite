7b9a3cdd39b1268a4bc1db8538f7a794cf82b3f5
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-04-24 11:14:10 +0900

    fix eslint warning

--- api/swa-api/index.ts
+++ api/swa-api/index.ts
@@ -1,8 +1,7 @@
-import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import { AzureFunction, Context } from '@azure/functions'
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
-  req: HttpRequest,
 ): Promise<void> {
   const now = new Date(Date.now())
   const responseMessage = now + ' です'
